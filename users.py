09e2c6fd531472bdd94bc0d7e2b4106b1a4299b4
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2024-06-09 12:14:00 +0300

    improved logout and session control

--- users.py
+++ users.py
@@ -7,18 +7,15 @@ def login(username, password):
     sql = "SELECT id, password FROM users WHERE username=:username"
     result = db.session.execute(text(sql), {"username":username})
     user = result.fetchone()
-    if not user:
-        return False
-    else:
-        if check_password_hash(user.password, password):
-            session["user_id"] = user.id
-            return True
-        else:
-            return False
+    if user and check_password_hash(user.password, password):
+        session["user_id"] = user.id
+        session["username"] = username
+        return True
+    return False
         
 
 def logout():
-    del session[user_id]
+    session.clear()
 
 
 def register(username, email, password): 
@@ -33,4 +30,4 @@ def register(username, email, password):
 
 
 def user_id():
-    return session.get("user_id", 0)
+    return session.get("user_id")
